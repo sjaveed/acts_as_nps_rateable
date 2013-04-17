f1de8d858eabb63223929f75de7881e05e45f6d3
Shahbaz Javeed <sjaveed@gmail.com>
2013-04-17 21:48:00 +0400

    * Fixed more typos

--- lib/acts_as_nps_rateable/instance_methods.rb
+++ lib/acts_as_nps_rateable/instance_methods.rb
@@ -33,7 +33,7 @@ module ActsAsNpsRateable::InstanceMethods
     nps_ratings.where(score: [7, 8]).size
   end
 
-  def detracters
+  def detractors
     nps_ratings.where('score <= ?', 6).size
   end
 
@@ -41,7 +41,7 @@ module ActsAsNpsRateable::InstanceMethods
     total_ratings = nps_ratings.size
     return 0 if total_ratings == 0
 
-    (promotors - detracters) * 100 / total_ratings
+    (promoters - detractors) * 100 / total_ratings
   end
 
 end
