a549ef9ce5f02868d4403dbdc5e9e45f51b22c38
Shahbaz Javeed <sjaveed@gmail.com>
2013-04-17 20:58:25 +0400

    * Changed the railtie hook from to_prepare to after_initialize

--- lib/acts_as_nps_rateable/railtie.rb
+++ lib/acts_as_nps_rateable/railtie.rb
@@ -1,7 +1,7 @@
 module ActsAsNpsRateable
   class Railtie < Rails::Railtie
     # Extend ActiveRecord::Base here and include ActsAsNpsRateable instance methods
-    config.to_prepare do
+    config.after_initialize do
       ActiveRecord::Base.send(:extend, ActsAsNpsRateable::Hook)
     end
   end
