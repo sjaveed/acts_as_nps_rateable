1f21fc239d602e964fb108be22b9d89d0701142e
Shahbaz Javeed <sjaveed@gmail.com>
2013-04-17 21:16:00 +0400

    * Changed the railtie method to do_prepare again * Added the class_name in the has_many relationship

--- lib/acts_as_nps_rateable/railtie.rb
+++ lib/acts_as_nps_rateable/railtie.rb
@@ -1,7 +1,7 @@
 module ActsAsNpsRateable
   class Railtie < Rails::Railtie
     # Extend ActiveRecord::Base here and include ActsAsNpsRateable instance methods
-    config.after_initialize do
+    config.to_prepare do
       ActiveRecord::Base.send(:extend, ActsAsNpsRateable::Hook)
     end
   end
