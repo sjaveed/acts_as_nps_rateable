9a55cf7c76c3366a89eafd1fe2d3d0b262662489
Shahbaz Javeed <sjaveed@gmail.com>
2015-07-16 17:22:06 -0400

    * Fixed the migration for v0.0.5 to be reversible

--- lib/generators/acts_as_nps_rateable/templates/03-migration.rb
+++ lib/generators/acts_as_nps_rateable/templates/03-migration.rb
@@ -1,6 +1,6 @@
 class ActsAsNpsRateableMigrationUpgrade005 < ActiveRecord::Migration
   def change
-    remove_index :nps_ratings, name: "acts_as_nps_rateable_unique_index"
+    remove_index :nps_ratings, column: [:nps_rateable_type, :nps_rateable_id, :user_id], unique: true, name: "acts_as_nps_rateable_unique_index"
 
     rename_column :nps_ratings, :user_id, :rater_id
     add_column :nps_ratings, :rater_type, :string, default: 'User'
